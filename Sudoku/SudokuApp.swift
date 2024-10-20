045c1fbaecd97f83146bdec68470303b4adbcd42
WALID SASSI <sassi.walid@outlook.com>
2024-10-20 10:07:04 +0200

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Walid SASSI on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SudokuTests/SudokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuTests.swift
+//  SudokuTests
+//
+//  Created by Walid SASSI on 20/10/2024.
+//
+
+import Testing
+@testable import Sudoku
+
+struct SudokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
